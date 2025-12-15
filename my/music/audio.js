d61cbfde661319760e4f8b6de3739157ef29b101
HaoleiQ@gmail.com <haoleiq@gmail.com>
2025-12-15 11:38:12 +0800

    Site updated: 2025-12-15 11:38:12

---
+++ my/music/audio.js
@@ -0,0 +1,31 @@
+var audio= [{
+    name: 'Wellerman',
+    artist: 'Wellerman',
+    url: '/my/cos/site/blog/mp3/Wellerman.mp3',
+    cover: '/my/cos/site/blog/mp3/Wellerman.jpg'
+},{
+    name: '大田後生仔 - 丫蛋蛋',
+    artist: '林啟得',
+    url: ' /my/cos/site/blog/mp3/datianhoushengzai.mp3',
+    cover: ' /my/cos/site/blog/mp3/datianhoushengzai.jpg'
+}, {
+    name: '画 - 赵雷',
+    artist: '赵小雷',
+    url: ' /my/cos/site/blog/mp3/hua-zhaolei.mp3',
+    cover: ' /my/cos/site/blog/mp3/hua-zhaolei.jpg'
+}, {
+    name: '烈火雄风',
+    artist: '吕继宏',
+    url: ' /my/cos/site/blog/mp3/liehuoxiongfeng.mp3',
+    cover: ' /my/cos/site/blog/mp3/liehuoxiongfeng.jpg'
+}, {
+    name: 'Immortals',
+    artist: 'Fall Out Boy',
+    url: ' /my/cos/site/blog/mp3/Immortals.mp3',
+    cover: ' /my/cos/site/blog/mp3/Immortals.jpg'
+}, {
+    name: '赵小雷',
+    artist: '赵小雷',
+    url: ' /my/cos/site/blog/mp3/zhaoxiaolei.mp3',
+    cover: ' /my/cos/site/blog/mp3/zhaoxiaolei.jpg'
+}];
